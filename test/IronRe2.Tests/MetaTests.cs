d5640e94db98848f62e6570f27224ca32d6ae227
Will Speak <will.speak@crispthinking.com>
2019-03-29 12:22:55 +0000

    Expose Version Information in Meta
    This static class contains the verison information for the library.

---
+++ test/IronRe2.Tests/MetaTests.cs
@@ -0,0 +1,34 @@
+using Xunit;
+using IronRe2;
+
+namespace IronRe2.Tests
+{
+    public class MetaTests
+    {
+        [Fact]
+        public void MetaGetVersionString()
+        {
+        //Given
+        
+        //When
+            var version = Meta.VersionString;
+        
+        //Then
+            Assert.Equal("0.0.0", version);
+        }
+
+        [Fact]
+        public void MetaGetVersion()
+        {
+        //Given
+        
+        //When
+            var version = Meta.Version;
+        
+        //Then
+            Assert.Equal(0, version.age);
+            Assert.Equal(0, version.current);
+            Assert.Equal(0, version.revision);
+        }
+    }
+}
