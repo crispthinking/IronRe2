150cfdd6cd3dcdebca0262287f284ea6cc29419d
Oscar Jargren <oscar.jargren@kroll.com>
2025-03-03 14:33:40 +0000

    Try

--- build.ps1
+++ build.ps1
@@ -6,7 +6,12 @@ $env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = '1'
 $env:DOTNET_CLI_TELEMETRY_OPTOUT = '1'
 $env:DOTNET_NOLOGO = '1'
 
-dotnet tool restore
+# Ensure Cake is up to date
+dotnet tool update Cake.Tool --global || true
+dotnet tool update Cake.Tool || true
+dotnet add package Cake.Core --version latest || true
+dotnet restore || true
+
 if ($LASTEXITCODE -ne 0) { exit $LASTEXITCODE }
 
 dotnet cake @args
