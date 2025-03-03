f8e8d1028610fc64403bd66d041b78e1d262bcd7
Oscar Jargren <oscar.jargren@kroll.com>
2025-03-03 13:22:58 +0000

    Try updating cake

--- build.sh
+++ build.sh
@@ -7,6 +7,14 @@ export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 
+# Ensure Cake is up to date
+dotnet tool update Cake.Tool --global || true
+dotnet tool update Cake.Tool || true
+dotnet add package Cake.Core --version latest || true
+dotnet restore || true
+
+# Restore tools (in case Cake needs dependencies)
 dotnet tool restore
 
+# Run Cake
 dotnet cake "$@"
